876bc0cb3e063722af8c3e3922e0166968f85d7c
231globus <231globus@gmail.com>
2023-03-13 00:13:52 +0300

    feat: init input value from state after component did mount

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
-class Search extends Component {
+type SearchState = {
+  input: string;
+};
+
+class Search extends Component<object, SearchState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+  }
+  componentDidMount() {
+    const inputValue = localStorage.getItem('input');
+    if (inputValue) {
+      this.setState({ input: inputValue });
+    }
+  }
   render() {
     return (
       <>
-        <input className="search" type="text" placeholder="Search card" />
+        <input
+          className="search"
+          type="text"
+          placeholder="Search card"
+          defaultValue={this.state.input}
+        />
       </>
     );
   }
