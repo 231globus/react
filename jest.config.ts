65718289cf950c295bdd2e349d8f06662d541013
231globus <231globus@gmail.com>
2023-03-12 15:10:35 +0300

    feat: added style

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
 };
