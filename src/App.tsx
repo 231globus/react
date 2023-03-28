6964caf8737a26502f2e1b9bfd60d7259703d757
231globus <231globus@gmail.com>
2023-03-29 01:07:48 +0300

    refactor: refactored app class component to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import ErrorPage from './pages/ErrorPage';
-import React, { Component } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import UsersPage from './pages/UsersPage';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
