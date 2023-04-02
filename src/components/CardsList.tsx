72b6de1993527701a1e2aee222ef00b449c62db5
231globus <231globus@gmail.com>
2023-04-02 03:47:18 +0300

    feat: added api

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,21 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardsItem from './CardsItem';
-import { products } from '../assets/dummy.json';
-import { Card } from 'types/types';
+import axios from 'axios';
+
+type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+type RickAndMorty = {
+  info: {
+    count: number;
+    pages: number;
+    next: string;
+    prev: string;
+  };
+  results: Character[];
+};
 
 function CardsList() {
-  const [cards] = useState<Card[]>(products);
+  const [character, setCharacter] = useState<RickAndMorty>();
+
+  async function getCharacter() {
+    axios.get('https://rickandmortyapi.com/api/character').then((res) => {
+      setCharacter(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
 
   return (
     <section className="content" data-testid="content">
-      {cards.map((value) => (
+      {character?.results.map((value) => (
         <CardsItem
           key={value.id}
-          title={value.title}
-          description={value.description}
-          price={value.price}
-          raiting={value.rating}
-          images={value.thumbnail}
+          title={value.name}
+          description={value.species}
+          price={value.id}
+          raiting={value.id}
+          images={value.image}
         />
       ))}
     </section>
