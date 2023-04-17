d0951b076b2f448e6d93594c7a7dfdfba5826cce
231globus <231globus@gmail.com>
2023-04-18 01:29:09 +0300

    fix: remove rtk query services

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { cardsListApi } from '../services/CardsListService';
 import { rootReducer } from './reducers';
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({ serializableCheck: false }).concat(cardsListApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
   });
 };
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { cardsListApi } from '../../services/CardsListService';
 import cardsReducer from './cards.reducer';
 import modalReducer from './modal.reducer';
 import usersReducer from './users.reducer';
@@ -8,5 +7,4 @@ export const rootReducer = combineReducers({
   cardsReducer,
   modalReducer,
   usersReducer,
-  [cardsListApi.reducerPath]: cardsListApi.reducer,
 });
